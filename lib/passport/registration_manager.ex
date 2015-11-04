37046eb00e0a9e1a475a618d5dd7d40cb26b8ebe
songomem <guillermo@origamicall.com>
2015-11-04 10:10:52 +0100

    add verify confirm password, and recaptcha

--- lib/passport/registration_manager.ex
+++ lib/passport/registration_manager.ex
@@ -1,17 +1,26 @@
 defmodule Passport.RegistrationManager do
   import Ecto.Changeset
   import Passport.Model
+  require Logger
 
-  def register(params) do
+  def register(params, :ok) do
+    Logger.info "Registro de usuario con captcha correcto"    
     changeset = user_model.changeset(user_model.__struct__, params)
-
     changeset = changeset
       |> validate_format(:email, ~r/@/)
       |> update_change(:email, &String.downcase/1)
+      |> confirm_password
       |> set_hashed_password
       |> unique_constraint(:email)
 
     repo.insert(changeset)
+  end
+  
+  def register(params, _) do
+    Logger.info "Registro de usuario con captcha incorrecto"
+    changeset = Ecto.Changeset.cast(struct(user_model), %{}, [])
+        |> add_error(:recaptcha, "incorrect")
+    {:error, changeset}
   end
 
   def downcase_email(changeset = %{params: %{"email" => email}}) do
@@ -19,11 +28,23 @@ defmodule Passport.RegistrationManager do
     |> put_change(:email, String.downcase(email))
   end
 
-  def set_hashed_password(changeset = %{params: %{"password" => password}}) when password != "" and password != nil do
+  def confirm_password(changeset = %{params: %{"password" => password, "confirmPassword" => confirmPassword}}) do
+    if password != confirmPassword do
+        changeset
+         |> add_error(:password, "have to match")
+     else
+      changeset
+    end
+  end
+  
+  def set_hashed_password(changeset = %{params: %{"password" => password}})
+                         when password != "" and password != nil  do
     hashed_password = Comeonin.Bcrypt.hashpwsalt(password)
     changeset
     |> put_change(:crypted_password, hashed_password)
   end
+   
+  
   def set_hashed_password(changeset) do
     changeset
     |> add_error(:password, "is required")
